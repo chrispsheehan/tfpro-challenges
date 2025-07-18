72cbd541da4f20a4b551c6dcd6d9f07eb1373995
chrispsheehan <chrispsheehan@gmail.com>
2025-07-18 12:04:04 +0100

    chore: imports via tf file

--- challenge-1/main.tf
+++ challenge-1/main.tf
@@ -22,6 +22,21 @@ resource "aws_iam_user" "lb" {
   name  = "${random_pet.this.id}-${var.org-name}-${count.index}"
 }
 
+import {
+  to = aws_iam_user.lb[0]
+  id = "trusting-camel-kplabs-0"
+}
+
+import {
+  to = aws_iam_user.lb[1]
+  id = "trusting-camel-kplabs-1"
+}
+
+import {
+  to = aws_iam_user.lb[2]
+  id = "trusting-camel-kplabs-2"
+}
+
 # This policy must be associated with all IAM users created through this code.
 
 resource "aws_iam_user_policy" "lb_ro" {
@@ -48,6 +63,17 @@ resource "aws_s3_bucket" "example" {
   bucket   = "${random_pet.this.id}-${each.value}"
 }
 
+import {
+  to = aws_s3_bucket.example["kplabs-1"]
+  id = "trusting-camel-kplabs-1"
+}
+
+import {
+  to = aws_s3_bucket.example["kplabs-2"]
+  id = "trusting-camel-kplabs-2"
+}
+
+
 resource "aws_s3_object" "object" {
   for_each = toset(var.s3_buckets)
   bucket   = aws_s3_bucket.example[each.key].id
@@ -58,6 +84,12 @@ resource "aws_security_group" "example" {
   name = var.sg_name
 }
 
+import {
+  to = aws_security_group.example
+  id = "sg-04d947f344404324b"
+}
+
+
 resource "aws_vpc_security_group_ingress_rule" "example" {
   security_group_id = aws_security_group.example.id
 
@@ -65,4 +97,9 @@ resource "aws_vpc_security_group_ingress_rule" "example" {
   from_port   = 80
   ip_protocol = "tcp"
   to_port     = 80
+}
+
+import {
+  to = aws_vpc_security_group_ingress_rule.example
+  id = "sgr-0145528e5e9c65cc1"
 }
